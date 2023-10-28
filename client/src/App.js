7885e98938c99112ad3ca057774970f626ae5dfe
Nhu Y <ledangnhuy16@gmail.com>
2023-10-28 15:33:13 +0700

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './App.scss';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <NavBar />
   );
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,43 +1,46 @@
-import React, { useState } from "react";
-import { BrowserRouter, Link } from "react-router-dom";
+import React from "react";
 import "./NavBar.scss";
-import { navItems } from "./NavItems";
-import Dropdown from "./Dropdown";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import logo from "../logo.png";
 import profileImage from "../profile-image.png";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function NavBar() {
-    const [dropdown, setDropdown] = useState(false);
   return (
-    <BrowserRouter basename="/">
-      <nav className="navbar">
-        <Link to="/" className="navbar-logo">
-          <img src={logo} alt="Logo" />
-        </Link>
-        <ul className="nav-items">
-          {navItems.map((item) => {
-            if (item.title === "Services") {
-              return (
-                <li
-                  key={item.id}
-                  className={item.cName}
-                  onMouseEnter={() => setDropdown(true)}
-                  onMouseLeave={() => setDropdown(false)}
-                >
-                  <Link to={item.path}>{item.title}</Link>
-                  {dropdown && <Dropdown />}
-                </li>
-              );
-            }
-            return (
-              <li key={item.id} className={item.cName}>
-                <Link to={item.path}>{item.title}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    </BrowserRouter>
+    <div className="container-navbar">
+      <Navbar.Brand href="#">
+        <img src={logo} alt="Logo" />
+      </Navbar.Brand>
+      <div className="nav">
+        <Nav.Link className="nav-item" href="#">HOME</Nav.Link>
+        <NavDropdown title="USER" id="navbarScrollingDropdown" className="nav-item">
+          <NavDropdown.Item href="#action3">User Management</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Right Assignment</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="PORT" id="navbarScrollingDropdown" className="nav-item">
+          <NavDropdown.Item href="#action3">Layout Management</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Port Assignment</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Port Scheduler</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="ADMIN" id="navbarScrollingDropdown" className="nav-item">
+          <NavDropdown.Item href="#action3">Administrator</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Audit Log</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Report</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Device Detection</NavDropdown.Item>
+        </NavDropdown>
+      </div>
+      <Dropdown>
+        <Dropdown.Toggle variant="link" id="dropdown-basic" caret={false}>
+          <img src={profileImage} alt="Profile" className="profile-image" />
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item eventKey="1">Change Password</Dropdown.Item>
+          <Dropdown.Item eventKey="2">Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
   );
 }
 
