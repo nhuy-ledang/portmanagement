f7604637bab52aeca2b30edca9dbf3fd93118d9c
Nhu Y <ledangnhuy16@gmail.com>
2023-10-29 22:08:29 +0700

    update Login

--- client/src/components/AccountDropdown.js
+++ client/src/components/AccountDropdown.js
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
-import { Navigate } from 'react-router-dom';
-import useToken from '../useToken';
+import React from "react";
 import { Link } from 'react-router-dom';
 export default function AccountDropdown() {
-  const { setToken } = useToken();
-  const [loggedOut, setLoggedOut] = useState(false);
-
-  const handleLogout = () => {
-    setToken("");
-    localStorage.clear();
-    setLoggedOut(true);
-  };
-
-  if (loggedOut) {
-    return <Navigate to="/login" replace={true} />;
-  }
-
   return (
     <div className="account-dropdown">
       <ul>
         <li><Link to='/change-password' className='nav-link'>Change Password</Link></li>
-        <li className='nav-link' onClick={handleLogout}>Logout</li>
+        <li className='nav-link'>Logout</li>
       </ul>
     </div>
   );
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,45 +1,55 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './Login.scss';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./Login.scss";
 
 async function loginUser(credentials) {
- return fetch('http://localhost:8080/api/login', {
-   method: 'POST',
-   headers: {
-     'Content-Type': 'application/json'
-   },
-   body: JSON.stringify(credentials)
- })
-   .then(data => data.json())
+  return fetch("http://localhost:8080/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
 }
 
 export default function Login({ setToken }) {
   const [administrator, setAdministrator] = useState();
   const [password, setPassword] = useState();
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const token = await loginUser({
-        administrator,
-      password
+      administrator,
+      password,
     });
     setToken(token);
-  }
+  };
 
-  return(
+  return (
     <div className="login-wrapper">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='Administrator' onChange={e => setAdministrator(e.target.value)} />
-          <input type="password" placeholder='Password' onChange={e => setPassword(e.target.value)} />
-        <div>
-          <button type="submit">Login</button>
-        </div>
-      </form>
+      <div className="login">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+        {/* <h1>Login</h1> */}
+          <input
+            type="text"
+            placeholder="Administrator"
+            onChange={(e) => setAdministrator(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          
+            <button className="btn-login" type="submit">LOGIN</button>
+          
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 Login.propTypes = {
-  setToken: PropTypes.func.isRequired
+  setToken: PropTypes.func.isRequired,
 };
