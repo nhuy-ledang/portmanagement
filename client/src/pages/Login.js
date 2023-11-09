5c7e58fe1074f212a33a9ea9a001e1562d92fe48
Nhu Y <ledangnhuy16@gmail.com>
2023-11-10 06:58:48 +0700

    no message

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 async function loginUser(credentials) {
   try {
     const response = await fetch(
-      "http://localhost:8080/api/login",
+      "https://hpid.homethang.duckdns.org/api/login",
       {
         method: "POST",
         headers: {
